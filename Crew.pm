56b31d15f9135a09bdcb160f456b92ac86a7c630
Сединкин Сергей <s.sedinkin@reg.ru>
2017-03-03 01:40:02 +0400

    Добавил проверку в хэш, изменил отступы, геттеры и сеттеры имеют одно имя

--- Crew.pm
+++ Crew.pm
@@ -3,79 +3,54 @@
 package Crew;
 
 use strict;
+use Data::Dumper;
+
 
 sub new {
-	my($class) = @_;
-	my $self = {
-        Name => '',
-        Rank => '',
-        Specialty => '',
-        Life_time => '',
-        Model => ''
+    my ($class) = @_;
+    my $self = {
+        name => '',
+        rank => '',
+        specialty => '',
+        life_time => '',
+        model => ''
     };
     bless $self, $class;
 }
 
 sub AUTOLOAD {
-    my @elements = qw(Name Rank Specialty Life_time Model);
+    my ( $self, $value ) = @_;
     our $AUTOLOAD;
-    # Устанавливаем
-    if ($AUTOLOAD =~ /::set_(\w+)$/ and grep $1 eq $_, @elements) {
-        my $field = ucfirst $1;
-        {
-        no strict 'refs';
-        #*{$AUTOLOAD} = sub { $_[0]->{$field} = $_[1] };
-        *{$AUTOLOAD} = sub {
-            my ( $self, $value ) = @_;
-            if ( $self->validation( $value, $field ) ) {
-                $self->{$field} = $value
-            }
-            else {
-                die "$field failed validation"; 
-            }
-        };        
+
+    if ( $AUTOLOAD =~ /::(\w+)$/ ) {
+        my $field = $1;
+        if ( @_ == 2 ) {
+            $self->validation( $field, $value );
+            $self->{$field} = $value;
         }
-        goto &{$AUTOLOAD};
+        return $self->{$field}; 
     }
-    # Получаем
-    if ($AUTOLOAD =~ /::get_(\w+)$/ and grep $1 eq $_, @elements) {
-        my $field = ucfirst $1;
-        {
-        no strict 'refs';
-        *{$AUTOLOAD} = sub { $_[0]->{$field} };
-        }
-        goto &{$AUTOLOAD};
+    else {
+        die "Вызван ошибочный метод $1\n";
     }
-    # Завершить работу с ошибкой, если передан некорректный метод
-    die "$_[0] вызван ошибочный метод $1\n";
 }
-
-
 
 sub validation { 
-  my ($self, $value, $field) = @_;
+    my ( $self, $field, $value ) = @_;
+
+    my %validation = (
+        name => '^[а-яА-ЯёЁ]+\s[а-яА-ЯёЁ]+\s[а-яА-ЯёЁ]+',
+        rank => '^\w+$',
+        specialty => '^(командир|механик-водитель|наводчик|заряжающий|радист)$',
+        life_time => '^\d+$',
+        model => '.*'
+    );
+
+    die "Не существует поле $field"
+        unless exists $validation{$field};
+    die "Ошибка валидации поля $field"
+        unless $value =~ $validation{$field};
+    return 1;
+} 
   
-  if ( $field eq 'Name' and $value =~ /^([йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮёЁ]+)\s([йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮёЁ]+)\s([йцукенгшщзхъфывапролджэячсмитьбюЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮёЁ]+)$/ ) {
-     # validate Name
-    return 1;
-  }
-  elsif ( $field eq 'Rank' and $value =~ /^\w+$/ ) {
-    # validate Rank
-    return 1;
-  }
-  elsif ( $field eq 'Specialty' and $value =~/командир|механик-водитель|наводчик|заряжающий|радист/ ) {
-    # validate Specialty
-    return 1;
-  }
-  elsif ( $field eq 'Life_time' and $value =~ /\d+/ ) {
-    # validate Life_time
-    return 1;
-  }
-  elsif ( $field eq 'Model' ) {
-    # validate Model
-    return 1;
-  }
-
-}
-
 1;
--- crew_member.pl
+++ crew_member.pl
@@ -1,33 +1,27 @@
 #!/usr/bin/perl
 
-# Устанавливаем или получаем поля: Name, Rank, Specialty, Life_time, Model
-# Получить поле Name: get_Name
-# Установить поле Name: set_Name('Пупкин Иван Ильич')
-
 use Crew;
 use Data::Dumper;
 
 
+
 my $crmem = Crew->new();
+
+
 # Проверяем поля объекта
 print Data::Dumper::Dumper( $crmem );
-
-# Устанавливаем поля
-$crmem->set_Name('Иванов Иван Иванович');
-# Проверяем, корректно ли установлены поля
+$crmem->name('Иванов Иван Иванович');
 print Data::Dumper::Dumper( $crmem );
-# Продолжаем устанавливать поля
-#$crmem->set_Name('absnext asd asd'); # Неправильно
-$crmem->set_Rank('superman'); # Правильно
+$crmem->rank('superman'); # Правильно
+#$crmem->rrank('superman'); # Неправильно
 # $crmem->set_Rank('abs 123'); # Неправильно
-$crmem->set_Specialty('командир'); # Правильно
+$crmem->specialty('командир'); # Правильно
 #$crmem->set_Specialty('мотострелок'); # Неправильно
-$crmem->set_Life_time('666'); # Правильно
+$crmem->life_time('666'); # Правильно
 #$crmem->set_Life_time('asd'); # Неправильно
-$crmem->set_Model('railgun');
+$crmem->model('railgun');
 print Data::Dumper::Dumper( $crmem );
 
 # Получаем поля
-print $crmem->get_Specialty."\n";
-print $crmem->get_Name."\n";
-
+print $crmem->specialty . "\n";
+print $crmem->name . "\n";
